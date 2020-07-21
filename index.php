d79a7fa0bf9108db9c0225b0b8549de4fc80e86b
M Laurin Penland <mlaurinpenland@ms-macbook-pro.home>
2020-07-21 19:22:30 -0400

    add blog posts

--- index.php
+++ index.php
@@ -33,17 +33,25 @@
 	  <li>“Viole(n)ts,” <a href="https://www.saltfront.org/product-page/saltfront-no-6"><i>saltfront: studies in human habit(at)</i></a>, Issue 7, Winter 2019</li>
 	  <li>“Strange Language,” <a href="http://palaverjournal.com/fall-2016/"><i>Palaver Journal</i></a>, Fall 2016<li>
 	  <li>“Making Ready,” <i>Lute & Drum</i>, Issue 7, 2016<li>
-	  </ul>
+	</ul>
 	<h3>Newspapers and Public Radio</h3>
 	<ul>
 	  <li>“South Carolina’s Negro Seamen Act,” <a href="https://www.heraldsun.com/"><i>Durham Herald-Sun</i></a>, December 11th, 2016</li>
 	  <li>“The Intentional Erasure of the Hayti District,” <a href="https://www.heraldsun.com/"><i>Durham Herald-Sun</i></a>, January 10th, 2016</li>
 	  <li>“Dr. Dog: The Black Hole of Modern Existence,” <a href="https://www.npr.org/search?query=laurin%20penland&page=1"><i>National Public Radio</i></a>, January 31st, 2012</li>
 	  <li>“Billy Edd Wheeler: 'I Can’t Rock Anymore, But I Can Still Roll,'” <a href="https://www.npr.org/search?query=laurin%20penland&page=1"><i>National Public Radio</i></a>, August 5th, 2012</li>
-      <li>“As N.C. Textile Jobs Fade, Denim Brightens Raleigh,” <a href="https://www.npr.org/2012/04/23/151189577/n-c-company-handcrafts-artisan-jeans-in-raleigh"><i>National Public Radio</i></a>, April 23rd, 2012</li>
-      <li>“From Knee-to-Knee to CD: The Evolution of Oral Tradition in Mountain Ballads,” <a href="https://www.npr.org/sections/therecord/2011/12/04/143010880/from-knee-to-knee-to-cd-the-evolution-of-oral-tradition-in-mountain-ballads"><i>National Public Radio</i></a>, December 4th, 2011</li>
-      <li>“John Prine: A Look Back at One Man’s War,” <a href="https://www.npr.org/2011/11/18/142490736/john-prine-a-look-back-at-one-mans-war"><i>National Public Radio</i></a>, November 18th, 2011</li>
+    <li>“As N.C. Textile Jobs Fade, Denim Brightens Raleigh,” <a href="https://www.npr.org/2012/04/23/151189577/n-c-company-handcrafts-artisan-jeans-in-raleigh"><i>National Public Radio</i></a>, April 23rd, 2012</li>
+    <li>“From Knee-to-Knee to CD: The Evolution of Oral Tradition in Mountain Ballads,” <a href="https://www.npr.org/sections/therecord/2011/12/04/143010880/from-knee-to-knee-to-cd-the-evolution-of-oral-tradition-in-mountain-ballads"><i>National Public Radio</i></a>, December 4th, 2011</li>
+    <li>“John Prine: A Look Back at One Man’s War,” <a href="https://www.npr.org/2011/11/18/142490736/john-prine-a-look-back-at-one-mans-war"><i>National Public Radio</i></a>, November 18th, 2011</li>
 	  <li>“You’ve Never Heard of Gram Parsons’ ‘Grievous Angel’?!” <a href="https://www.npr.org/sections/allsongs/2011/09/18/140546110/youve-never-heard-gram-parsons-grievous-angel"><i>National Public Radio</i></a>, September 18th, 2011</li>
+	</ul>
+    <h3>Blog Posts</h3>
+		<ul>
+			<li>“Diary Foreshadows Conviction for Involvement in the Slave Trade,” <a href="https://blogs.library.duke.edu/rubenstein/2020/06/23/diary-foreshadows-conviction-for-involvement-in-slave-trade/"<i>The Devil's Tale: Dispatches from the David M.Rubenstein Rare Book & Manuscript Library</i></a>, June 23rd, 2020</li>
+			<li>“'The Arm of Justice Cannot—Will Not Sleep': Radical Republicans during Reconstruction in the South”, <a href="https://blogs.library.duke.edu/rubenstein/2020/02/07/the-arm-of-justice-cannot-will-not-sleep-radical-republicans-during-reconstruction-in-the-south/"><i>The Devil's Tale: Dispatches from the David M.Rubenstein Rare Book & Manuscript Library</i></a>, February 7th, 2020</li>
+			<li>“Civil War Letters Grapple with Gender, Interracial Marriage, and Working-Class Life,” <a href="https://blogs.library.duke.edu/rubenstein/2018/06/28/civil-war-letters-grapple-with-gender-interracial-marriage-and-working-class-life/"><i>The Devil's Tale: Dispatches from the David M.Rubenstein Rare Book & Manuscript Library</i></a>, June 28th, 2018</li>
+			<li>“Rob Amberg: Forty Years in Appalachia,” <a href="https://blogs.library.duke.edu/rubenstein/2015/11/10/rob-amberg-forty-years-in-appalachia/"><i>The Devil's Tale: Dispatches from the David M.Rubenstein Rare Book & Manuscript Library</i></a>, November 10th, 2015</li>
+		</ul>
 	</article>
 
 
