c6ea7af691f7a2336b2848892cfe184d54afdfae
M Laurin Penland <mlaurinpenland@ms-macbook-pro.home>
2021-07-21 21:29:54 -0400

    Fix syntax error in header.

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 
-<body>
+
 
 <header>
 
@@ -8,15 +8,12 @@
 <!-- Top Navigation Menu -->
 <nav>
 	<ul>
-		<li><a href=index.php class="active"><i class="fas fa-home"></i>Home</a></li>
-  	<li><a href=writer.php>Writer</a></li>
-		<li><a href=webdeveloper.php>Web Developer</a></li>
-		<li><a href=ExquisiteCorpse.php>Exquisite Corpse</a></li>
+		<li><a href="index.php" class="active"><i class="fas fa-home"></i>Home</a></li>
+  	<li><a href="writer.php">Writer</a></li>
+		<li><a href="webdeveloper.php">Web Developer</a></li>
+		<li><a href="ExquisiteCorpse.php">Exquisite Corpse</a></li>
 </ul>
 </nav>
 
 
 </header>
-</body>
-
-</html>
