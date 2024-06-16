f518452764e1edc925a7c8fce2de7c23e3687955
Laurin Penland <lpenland@brynmawr.edu>
2024-06-16 09:02:20 -0400

    updating web developer bio

--- webdeveloper.php
+++ webdeveloper.php
@@ -18,65 +18,62 @@
 
 
 
-	<body class="web-developer">
-		<?php
-			include('includes/header.php');
-		?>
-  <div class="name web-developer-name">
-  <h1>Laurin Penland</h1>
-  </div>
+  <body class="web-developer">
+	<?php
+		include('includes/header.php');
+	?>
+    <div class="name web-developer-name">
+      <h1>Laurin Penland</h1>
+    </div>
 
 	<article>
-	<h2>Web Developer</h2>
+	  <h2>Web Developer</h2>
 
-	<p>I love to build beautiful, efficient, and responsive websites.</p>
+	  <p>I love to build accessible, responsive, and beautiful websites.</p>
 
-	<p>Much of my work has been related to libraries and archival collections. One of my recent projects was to improve user experience in the <a href="https://archives.lib.duke.edu/">Rubenstein Library's collection guide platform</a>. The platform is based on the open source projects <a href="https://library.stanford.edu/projects/arclight">ArcLight</a> and <a href="https://projectblacklight.org/">BlackLight</a>. <a href="v1.3.0_arclight_release_notes.pdf">Click to view release notes for my latest contributions to the site.</a>
+	  <p>Much of my work has been related to libraries and archival collections. I currently work as a web developer for the TriCollege Consortium (Bryn Mawr, Haverford, and Swarthmore libraries). One of my recent projects was to improve the discoverability of special collections materials by integrating ArchivesSpace with Alma. If you're curious about the project, you can watch a <a href="https://www.youtube.com/watch?v=yxJ_vVuqS-4"> webinar </a> on the ArchivesSpace YouTube channel.</p>
 
-	<p>I have experience working on sites that use a variety of frameworks including the MEAN stack, the LAMP stack, and Ruby on Rails. Future goals include becoming more expert at these frameworks, writing more tests (including creating automated accessibility tests), and learning more about DevOps tools such as Docker and Dependabot.</p>
+	  <p>I have experience working on sites that use a variety of frameworks including Ruby on Rails, MEAN stack, the LAMP stack, and Drupal. Future goals include becoming more expert at these frameworks, writing more tests (including creating automated accessibility tests), and learning more about DevOps tools.</p>
 
-	<h2>Fluencies</h2>
-	<ul class="fluencies">
-		<li>Programming languages: HTML5, CSS3, JavaScript, TypeScript, SQL, PHP, Python, Swift</li>
-		<li>Databases: Access, Oracle, mySQL, MongoDB</li>
-		<li>Frameworks: MEAN stack (MongoDB, Node.js, Angular, and Express)</li>
+	  <h2>Fluencies</h2>
+	  <ul class="fluencies">
+		<li>Programming languages: HTML5, CSS3, JavaScript, Ruby, TypeScript, SQL, PHP, Python</li>
+		<li>Databases: Access, Oracle, mySQL, MongoDB, PostgreSQL</li>
+		<li>Frameworks: Rails, MEAN stack (MongoDB, Node.js, Angular, and Express), LAMP stack, Drupal</li>
 		<li>Operating systems: Mac, Microsoft, Linux, Unix</li>
-		<li>Other: Git, GitHub, GitLab, Jira, AdobeXD, Adobe Dreamweaver, Photoshop, Postman, RSpec, axe DevTools</li>
-	</ul>
+		<li>Other: Git, GitHub, GitLab, Jira, Docker, AdobeXD, Photoshop, Postman, RSpec, axe DevTools</li>
+	  </ul>
 
-  <h2>Portfolio</h2>
-  <p>You can view a few of my projects on <a href="https://github.com/laurinpenland">GitHub.</a></p>
+    <h2>Portfolio</h2>
+    <p>You can view a few of my personal projects on <a href="https://github.com/laurinpenland">GitHub.</a></p>
 	<h3>Finding Family in Archives at Duke</h3>
 	<p>For a capstone project in an advanced CSS and HTML class, I created <a href="https://laurinpenland.github.io/finding-family-duke-archives.io/">a website that helps people research their ancestors in the archives at Duke</a>. The website is meant to be warm and welcoming, especially for those embarking on their search for the first time. The design is inspired by photographs and letters found at the library.</p>
 	<p><a href="https://github.com/laurinpenland/finding-family-duke-archives.io">View the repository on GitHub.</a></p>
 
 
 	<figure class="portfolio-image">
-	<a href= "https://github.com/laurinpenland/finding-family-duke-archives" target="_blank">
-	<img src="./images/FindingFamily.jpg" class="portfolio-image" alt="home page for Finding Family in Duke Archives website"/>
-	</a>
-	<figcaption>Website that helps researchers find family members in the archives at Duke.</figcaption>
-</figure>
+	  <a href= "https://github.com/laurinpenland/finding-family-duke-archives" target="_blank">
+	  <img src="./images/FindingFamily.jpg" class="portfolio-image" alt="home page for Finding Family in Duke Archives website"/>
+	  </a>
+	  <figcaption>Website that helps researchers find family members in the archives at Duke.</figcaption>
+    </figure>
 	<h3>Archival Supplies</h3>
 	<p>For a capstone project in advance scripting and markup, I built a website using the MEAN (MongoDB, Express, Angular, and Node.js) stack. I used Express to create an API, and the site includes reactive forms, behavior subjects, and observables. For the design, I used Bootstrap, Angular Material, and custom CSS. The purpose of the site is to facilitate supply ordering.</p>
 	<p><a href="https://github.com/laurinpenland/archival-supplies">View the repository on Github.</a></p>
 	<figure class="portfolio-image">
-	<a href="https://github.com/laurinpenland/archival-supplies" target="_blank">
+	  <a href="https://github.com/laurinpenland/archival-supplies" target="_blank">
 		<img src="./images/mean-stack-website.png" class="portfolio-image" alt="home page for Archival Supplies website"/>
-	</a>
-	<figcaption>Website for archival products used by the Rubenstein Library.</figcaption>
-</figure>
+	  </a>
+	  <figcaption>Website for archival products used by the Rubenstein Library.</figcaption>
+    </figure>
 
-	</article>
+  </article>
 
 
-	<footer>
-		<p>Website created by Laurin Penland. <a href="mailto:laurinpenland@laurinpenland.com">Contact Laurin.</a>
-		</p>
-		<p>The background photograph is an altered digital image of "Equivalents," 1924, by Alfred Stieglitz, courtesy of the <a href="http://www.getty.edu/art/collection/objects/62747/alfred-stieglitz-equivalents-american-1924/" target="_blank"><i>Getty's Open Content Program</i></a>.
-		</p>
+  <footer>
+	<p>Website created by Laurin Penland. <a href="mailto:laurinpenland@laurinpenland.com">Contact Laurin.</a></p>
+	<p>The background photograph is an altered digital image of "Equivalents," 1924, by Alfred Stieglitz, courtesy of the <a href="http://www.getty.edu/art/collection/objects/62747/alfred-stieglitz-equivalents-american-1924/" target="_blank"><i>Getty's Open Content Program</i></a>.</p>
 	</footer>
-	</body>
-
+  </body>
 
 </html>
